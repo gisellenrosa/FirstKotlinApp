0afc73c81f9a84d65285f0ee233f659115cff730
Giselle Nepomuceno Rosa <giselle.rosa@grupoboticario.com.br>
2021-08-04 12:30:13 -0300

    Primeiro Projeto Kotlin

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,10 @@
+package myFirstSpringKotlinApp
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class MyApplication
+
+fun main() {
+  runApplication<MyApplication>()
+}
